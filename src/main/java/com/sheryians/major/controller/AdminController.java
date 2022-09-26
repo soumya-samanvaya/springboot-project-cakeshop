4114d1a73523c9aed6f7a890f15b7aefc49dd54e
Soumya Samanvaya <soumya.samanvaya98@gmail.com>
2022-09-26 13:03:45 +0530

    initial commit

---
+++ src/main/java/com/sheryians/major/controller/AdminController.java
@@ -0,0 +1,21 @@
+package com.sheryians.major.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AdminController {
+
+    @GetMapping("/admin")
+    public String adminHome(){
+        return "adminHome";
+    }
+    @GetMapping("/admin/categories")
+    public String getCat(){
+        return "categories";
+    }
+    @GetMapping("/admin/categories/add")
+    public String getCatAdd(){
+        return "categoriesAdd";
+    }
+}
